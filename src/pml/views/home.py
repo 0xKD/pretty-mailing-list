bec78935222ff686f417c288ca86241b349fd5bd
Kedar <kedardeore@gmail.com>
2021-04-26 18:07:51 +0530

    Format timestamp in cache info-box
    - Minor styling updates

--- src/pml/views/home.py
+++ src/pml/views/home.py
@@ -1,3 +1,6 @@
+import timeago
+from datetime import datetime
+import pytz
 from urllib.parse import unquote, urlparse
 
 from pyramid.httpexceptions import HTTPServiceUnavailable, HTTPFound
@@ -33,7 +36,9 @@ def view_thread(request):
         "contents": thread.content,
         "original": src,
         "cached": thread.cached,
-        "pull_time": thread.get_pull_time()
+        "pull_time": thread.get_pull_time().isoformat(),
+        "pull_time_str": timeago.format(thread.get_pull_time(),
+                                        datetime.utcnow().replace(tzinfo=pytz.UTC))
     }
 
 
